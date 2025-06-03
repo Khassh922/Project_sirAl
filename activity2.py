a56cac2f95457f7edbaa24baa082e405a1a33b03
Khassh922 <johnlouisediaz65@gmail.com>
2025-06-03 14:33:02 +0800

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,28 @@
+print("\n---    Population in 2024  ---")
+san_nicolas = int(input("Baragay San nicolas how many people?:"))
+magdalo = int(input("Baragay Magdalo how many people? :"))
+gustillo = int(input("Baragay Gustillo how many people?:"))
+mabini = int(input("Baragay Mabini how many people?:"))
+baldoza = int(input("Baragay Baldoza how many people?:"))
+
+
+print("\n---2025 in Lapaz Population ---")
+
+
+barangay_magdalo = 560
+barangay_san_nicolas = 1200
+barangay_baldoza = 1600
+barangay_gustillo = 2300
+barangay_mabini = 240
+print(f"Baragay Magdalo:",{barangay_magdalo})
+print(f"Baragay San nicolas:",{barangay_san_nicolas})
+print(f"Baragay Mabini:",{barangay_baldoza})
+print(f"Baragay Baldoza:",{barangay_magdalo})
+print(f"Baragay Gustillo:",{barangay_gustillo})
+
+kill = (barangay_magdalo + barangay_san_nicolas + barangay_baldoza + barangay_gustillo + barangay_mabini)
+print("2024 total:",kill)
+fell = (san_nicolas + magdalo + gustillo + mabini + baldoza)
+print("2025 total:",fell)
+lol = (kill - fell)
+print("total sum population:",lol)
